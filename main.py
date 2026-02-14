33b80afa3c654654e7fd551eb454183f3a40ad0c
efekaanefe <efekaanefe1905@gmail.com>
2026-02-14 16:53:57 +0300

    examle depth estimation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from scripts.visual_odometry import VisualOdometry
 from scripts.visualizer import Visualizer
-from scripts.utils import calculate_trajectory_error
+from scripts.utils import calculate_trajectory_error, load_images, load_projection_matrices
+from scripts.depth_estimation import estimate_depth
+
+import os
 
 
-
-
-if __name__ == "__main__":
-    vo = VisualOdometry(data_dir="data/KITTI/2", is_left_camera = True, n_features=3000)
+def run_monocular_visual_odometry(data_dir="data/KITTI/2", is_left_camera = True, n_features=3000):
+    vo = VisualOdometry(data_dir, is_left_camera, n_features)
     visualizer = Visualizer()
 
     est_poses = vo.estimate_trajectory()
@@ -15,3 +16,29 @@ if __name__ == "__main__":
     error = calculate_trajectory_error(est_poses, vo.gt_poses)
     visualizer.plot_trajectory_error(error)
     print(f"Average Trajectory Error: {error.mean():.4f} meters")
+
+
+
+def run_depth_estimation():
+    data_dir = "data/KITTI/1"
+    
+    images_left = load_images(data_dir, is_left_camera=True)
+    images_right = load_images(data_dir, is_left_camera=False)
+    
+    calib_file = os.path.join(data_dir, "calib.txt")
+    calib = load_projection_matrices(calib_file)
+    P_left = calib["left"]
+    P_right = calib["right"]
+
+    image_index = 20
+    depth_map = estimate_depth(images_left[image_index], images_right[image_index], P_left, P_right)
+
+    visualizer = Visualizer()
+    visualizer.visualize_depth_map(depth_map)
+
+   
+
+if __name__ == "__main__":
+    # run_monocular_visual_odometry()
+    run_depth_estimation()
+
